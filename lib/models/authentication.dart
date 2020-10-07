f89dcd014c60b65fdb61a0b411ab829273a2240c
crazy6901 <harshkothari17@gmail.com>
2020-10-07 20:40:42 +0530

    Login/register App

--- lib/models/authentication.dart
+++ lib/models/authentication.dart
@@ -9,7 +9,7 @@ import 'package:login_form/models/error.dart';
 class Authentication with ChangeNotifier {
   Future<void> register(String email, String password) async {
     const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDvo2-OUXQF625qTRgpwu32g8h7aYGhDko';
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]';
 
     final response = await http.post(url,
         body: json.encode({
@@ -24,7 +24,19 @@ class Authentication with ChangeNotifier {
   //login
   Future<void> login(String email, String password) async {
     const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDvo2-OUXQF625qTRgpwu32g8h7aYGhDko';
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]';
+
+//Register-Login-App-Flutter
+//
+//Simple Register/Login app in flutter/firebase Step 1: Clone this file in your desired folder
+//
+//Step2: pub get
+//
+//Step 3: Make a project in Firebase console
+//
+//Step 4: Copy the API_KEY and place in it [API_KEY] in authentication.dart
+//
+//step 5: run the app
 
     try {
       final response = await http.post(url,
